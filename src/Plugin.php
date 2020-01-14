0e0dbcd47a8552652352cfe874272690a044f848
Thomas Körner <t.koerner@heimrich-hannot.de>
2020-01-14 15:36:45 +0100

    added initial bundle setup

---
+++ src/Plugin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2020 Heimrich & Hannot GmbH
+ *
+ * @author  Thomas Körner <t.koerner@heimrich-hannot.de>
+ * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ */
+
+
+namespace HeimrichHannot\DatamapsBundle\ContaoManager;
+
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use HeimrichHannot\DatamapsBundle\ContaoDatamapsBundle;
+
+class Plugin implements BundlePluginInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ContaoDatamapsBundle::class)->setLoadAfter(ContaoCoreBundle::class)
+        ];
+    }
+}
